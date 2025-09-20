7aa7e13985e454005eb65e3a461d5d4278433869
Homero Salazar <yo@homerosalazar.com>
2025-09-20 10:50:55 -0600

    adding basic django-environ support

--- {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@%eoort6#kt!i%*77udxt$&s8ti4rz(^=rg-081)fkb5z1l@dl"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = []
 
