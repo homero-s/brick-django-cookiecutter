1dc9f54dd537a310ce9b46dac91740e2dc298ee9
Homero Salazar <yo@homerosalazar.com>
2025-09-20 11:35:21 -0600

    adding post gen hook to generate django secret key env var

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,7 +2,17 @@ import json
 from pathlib import Path
 import shutil
 import subprocess
+import random
 import sys
+import string
+
+try:
+    # Inspired by
+    # https://github.com/django/django/blob/main/django/utils/crypto.py
+    random = random.SystemRandom()
+    using_sysrandom = True
+except NotImplementedError:
+    using_sysrandom = False
 
 
 def _yaml_escape(value: str) -> str:
@@ -107,6 +117,64 @@ def remove_uv_compose_dir() -> None:
             pass
 
 
+def generate_random_string(
+    length, using_digits=False, using_ascii_letters=False, using_punctuation=False
+):  # noqa: FBT002
+    """
+    Example:
+        opting out for 50 symbol-long, [a-z][A-Z][0-9] string
+        would yield log_2((26+26+50)^50) ~= 334 bit strength.
+    """
+    if not using_sysrandom:
+        return None
+
+    symbols = []
+    if using_digits:
+        symbols += string.digits
+    if using_ascii_letters:
+        symbols += string.ascii_letters
+    if using_punctuation:
+        all_punctuation = set(string.punctuation)
+        # These symbols can cause issues in environment variables
+        unsuitable = {"'", '"', "\\", "$", "#"}
+        suitable = all_punctuation.difference(unsuitable)
+        symbols += "".join(suitable)
+    return "".join([random.choice(symbols) for _ in range(length)])
+
+
+def set_flag(file_path: Path, flag, value=None, formatted=None, *args, **kwargs):
+    if value is None:
+        random_string = generate_random_string(*args, **kwargs)
+        if random_string is None:
+            print(
+                "We couldn't find a secure pseudo-random number generator on your "
+                f"system. Please, make sure to manually {flag} later.",
+            )
+            random_string = flag
+        if formatted is not None:
+            random_string = formatted.format(random_string)
+        value = random_string
+
+    with file_path.open("r+") as f:
+        file_contents = f.read().replace(flag, value)
+        f.seek(0)
+        f.write(file_contents)
+        f.truncate()
+
+    return value
+
+
+def set_django_secret_key(file_path: Path):
+    return set_flag(
+        file_path,
+        "supersecret",
+        length=64,
+        using_digits=True,
+        using_ascii_letters=True,
+        using_punctuation=True,
+    )
+
+
 def main() -> None:
     create_config_file()
 
@@ -115,6 +183,8 @@ def main() -> None:
     setup_dependencies()
 
     remove_uv_compose_dir()
+
+    set_django_secret_key(Path(".env"))
 
 
 if __name__ == "__main__":
