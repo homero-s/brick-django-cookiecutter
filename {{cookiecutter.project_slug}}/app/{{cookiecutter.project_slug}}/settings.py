cacf15f20a3d28608fe942059e44ff2a6c74760a
Homero Salazar <yo@homerosalazar.com>
2025-09-28 12:30:32 -0600

    adding domain from cookiecutter to django allowed hosts

--- {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
 
 # Application definition
 
